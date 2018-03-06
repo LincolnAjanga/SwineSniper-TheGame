735eed87b9e4ba6fc825bf754c9b774b0c043e80
T-Boya <t.osewe1@gmail.com>
2018-03-06 12:35:45 +0300

    removed commented out code, it is now outdated and useless

--- JS/scripts.js
+++ JS/scripts.js
@@ -34,7 +34,7 @@ var interval = setInterval(function() {
         bgMusic.pause();
         audio.pause();
         gameOver.play();
-        
+
         $( document.body ).click(function() {
           gunshot.pause()
         });
@@ -66,42 +66,3 @@ $(document).mousemove(function(e) {
     });
 });
 
-
-
-// document.getElementById('gameOver').style.visibility = 'hidden';
-// var seconds_left = 11;
-// var interval = setInterval(function() {
-//     document.getElementById('timer_div').innerHTML = --seconds_left;
-
-//     if (seconds_left <= 10) {
-//         var ticking = new Audio('assets/ticking.mp3');
-//         audio.loop = false;
-//         audio.play();
-//     }
-
-//     if (seconds_left <= 0){
-//         document.getElementById('timer_div').innerHTML = 'Time Up!';
-//         clearInterval(interval);
-//         document.getElementById('gameOver').style.visibility = 'visible';
-//         document.getElementById('togglemusic').style.visibility = 'hidden';
-//         bgMusic.pause()
-//         ticking.pause()
-//     }
-// }, 1000);
-
-// var bgMusic = document.getElementById("bgMusic");
-// var isPlaying = false;
-
-// function togglePlay() {
-//   if (isPlaying) {
-//     bgMusic.pause()
-//   } else {
-//     bgMusic.play();
-//   }
-// };
-// bgMusic.onplaying = function() {
-//   isPlaying = true;
-// };
-// bgMusic.onpause = function() {
-//   isPlaying = false;
-// };
